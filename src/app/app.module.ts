f0b9e8674f9860f02bf43f43e2bc039b9a655328
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-12-08 16:04:16 -0600

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BusRouteComponent } from './bus-route/bus-route.component';
 
@@ -12,7 +11,7 @@ import { BusRouteComponent } from './bus-route/bus-route.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
